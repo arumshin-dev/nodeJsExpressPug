05108889ffbbd7ce4221d0b56273f65e6dce8d6f
arumshin-dev <159020719+arumshin-dev@users.noreply.github.com>
2024-04-13 11:07:12 +0000

    babel.config.json>babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    //'@babel/preset-react'
+  ],
+  // plugins: [
+  //   '@babel/plugin-transform-runtime'
+  // ]
+  // 아래는 예제용 플러그인 설정입니다.
+  // plugins: ['@babel/plugin-syntax-dynamic-import']
+};
