a48ea544a8fec9e1f054744a2f63af7139d580cc
arumshin-dev <159020719+arumshin-dev@users.noreply.github.com>
2024-04-23 01:53:26 +0000

    room disconneting

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -70,6 +70,9 @@ socket.on("welcome", () => {
   addMessage("Someone joined!");
 });
 
+socket.on("bye", () => {
+  addMessage("Someone left! ㅠㅠ");
+});
 /*
 const socket = new WebSocket(`wss://${window.location.host}`);
 const messageList = document.querySelector("ul");
