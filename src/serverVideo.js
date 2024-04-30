fa739b04176e560b29b8d1698cc472216089656d
arumshin-dev <159020719+arumshin-dev@users.noreply.github.com>
2024-04-30 00:43:07 +0000

    navigator.mediaDevices.getUserMedia

---
+++ src/serverVideo.js
@@ -0,0 +1,19 @@
+import http from "http";
+
+import SocketIO from "socket.io";
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+
+app.set("views", __dirname + "/views");
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => res.render("homeVideo"));
+
+app.get("/*", (req, res) => res.redirect("/"));
+
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
